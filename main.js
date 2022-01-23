022866b96d549e82fe3306746d1eeca30740ecf3
Jordan Prescott <prescottjp@icloud.com>
2022-01-22 21:01:37 -0600

    changed the color of the header.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const calculateTip = () => {
-    let sum = document.getElementById("amount-input").value;
-    const tipPercentage = sum * 0.20;
-   if(tipPercentage == 0 ){
-    document.getElementById("percentage-display").innerHTML = "You need to enter a amount."
-   }else{
-    document.getElementById("percentage-display").innerHTML = `Your tip will be ${tipPercentage}$`;
-   }
+  const sum = document.getElementById("amount-input").value;
+  const tipPercentage = sum * 0.2;
+  if (tipPercentage == 0) {
+    document.getElementById("percentage-display").innerHTML =
+      "You need to enter a amount.";
+  } else {
+    document.getElementById(
+      "percentage-display"
+    ).innerHTML = `Your tip will be ${tipPercentage}$`;
+  }
 };
-
